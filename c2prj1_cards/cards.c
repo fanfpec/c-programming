9b42d29bb4044dfdf93cb74a41c79a9b15713375
Coursera Learner <nobody@nowhere.nul>
2019-09-11 16:41:05 +0000

    revise

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -67,31 +67,31 @@ void print_card(card_t c) {
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
-  if ( value_let == 'J' )//|| value_let == 'j' )
+  if ( value_let == 'J' || value_let == 'j' )
     temp.value = 11;
-  else if ( value_let == 'Q' )//|| value_let == 'q' )
+  else if ( value_let == 'Q' || value_let == 'q' )
     temp.value = 12;
-  else if ( value_let == 'K')// || value_let == 'k' )
+  else if ( value_let == 'K' || value_let == 'k' )
     temp.value = 13;
-  else if ( value_let == 'A' )//|| value_let == 'a' )
+  else if ( value_let == 'A' || value_let == 'a' )
     temp.value = 14;
   else if ( value_let == '0' )
     temp.value = 10;
   else if ( value_let > '1' && value_let <= '9' )
     temp.value = value_let - '0';
   else
-    printf("Invalid value input!"); return(EXIT_FAILURE);
+    printf("Invalid value input!"); exit(EXIT_FAILURE);
 
   switch( suit_let ){
-    // case 'S': temp.suit = SPADES; break;
-    //case 'H': temp.suit = HEARTS; break;
-    //case 'D': temp.suit = DIAMONDS; break;
-    //case 'C': temp.suit = CLUBS; break;
+   case 'S': temp.suit = SPADES; break;
+   case 'H': temp.suit = HEARTS; break;
+   case 'D': temp.suit = DIAMONDS; break;
+   case 'C': temp.suit = CLUBS; break;
   case 's': temp.suit = SPADES; break;
   case 'h': temp.suit = HEARTS; break;  
   case 'd': temp.suit = DIAMONDS; break;
   case 'c': temp.suit = CLUBS; break;
-  default: printf("Invalid suit input!"); return(EXIT_FAILURE);
+  default: printf("Invalid suit input!"); exit(EXIT_FAILURE);
   }
   
   return temp;
