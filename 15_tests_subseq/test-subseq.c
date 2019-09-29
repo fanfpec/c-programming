caff82b5089ed646f7f1496349841711b7e3f5ff
Coursera Learner <nobody@nowhere.nul>
2019-09-29 19:32:45 +0000

    did something

---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+size_t doTest(int * array, size_t n) {
+  printf("maxSeq(");
+  if (array == NULL) {
+    printf("NULL");
+  }
+  else {
+    printf("{");
+    for (int i =0; i < n; i++) {
+      printf("%d", array[i]);
+      if (i < n -1) {
+	printf(", ");
+      }
+    }
+    printf("}");
+  }
+  printf(", %d) is \n", n);
+  size_t len = maxSeq (array, n);
+  if (len == NULL) {
+    printf("NULL\n");
+  }
+  else {
+    printf("%d\n", len);
+  }
+  return len;
+}
+
+int main(void) {
+  int array1[] = { 77, 33, 19, 99, 42, 6, 27, 4}; //2
+  int array2[] = { -3, -42, -99, -1000, -999, -88, -77}; //4
+  int array3[] = { 425, 59, -3, 77, 0, 36}; //2
+
+  doTest (array1, 8);
+  doTest (array2, 7);
+  doTest (array3, 6);
+  doTest (NULL, 0);
+  doTest (array1, 0);
+
+  return EXIT_SUCCESS;
+}
