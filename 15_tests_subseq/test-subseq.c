2abd09161474e89c26bcef8e9ae3b9f45df3e237
Coursera Learner <nobody@nowhere.nul>
2019-09-30 16:32:16 +0000

    maxSeq

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -3,31 +3,31 @@
 
 size_t maxSeq(int * array, size_t n);
 
-size_t doTest(int * array, size_t n) {
-  printf("maxSeq(");
-  if (array == NULL) {
-    printf("NULL");
-  }
-  else {
-    printf("{");
-    for (size_t i =0; i < n; i++) {
-      printf("%d", array[i]);
-      if (i < n -1) {
-	printf(", ");
-      }
-    }
-    printf("}");
-  }
-  printf(", %d) is \n", n);
-  size_t len = maxSeq (array, n);
-  if (len == NULL) {
-    printf("NULL\n");
-  }
-  else {
-    printf("%d\n", len);
-  }
-  return len;
-}
+//size_t doTest(int * array, size_t n) {
+//  printf("maxSeq(");
+//  if (array == NULL) {
+//    printf("NULL");
+//  }
+//  else {
+//    printf("{");
+//    for (size_t i =0; i < n; i++) {
+//      printf("%d", array[i]);
+//     if (i < n -1) {
+//	printf(", ");
+//      }
+//    }
+//    printf("}");
+//  }
+//  printf(", %d) is \n", n);
+//  size_t len = maxSeq (array, n);
+//  if (len == NULL) {
+//    printf("NULL\n");
+//  }
+//  else {
+//    printf("%d\n", len);
+//  }
+//  return len;
+//}
 
 int main(void) {
   int array1[] = { 77, 33, 19, 99, 42, 6, 27, 4,1111}; //2
@@ -36,13 +36,13 @@ int main(void) {
   int array4[] = { 1,1,2,3,4,10};
   int array5[] = { 1,1,1};
   //int array6[] = { -9,-8};
-  if( doTest (array1, 9) != 2 ) return EXIT_FAILURE;
-  if( doTest (array2, 8) != 5 ) return EXIT_FAILURE;
+  if( maxSeq(array1, 9) != 2 ) return EXIT_FAILURE;
+  if( maxSeq(array2, 8) != 5 ) return EXIT_FAILURE;
   //if( doTest (array3, 6) != 2 ) return EXIT_FAILURE;
-  if( doTest (NULL, 0) != 0 ) return EXIT_FAILURE;
-  if( doTest (array1, 0) != 2 ) return EXIT_FAILURE;
-  if( doTest (array4, 6) != 5 ) return EXIT_FAILURE;
-  if( doTest (array4, 5) != 5 ) return EXIT_FAILURE;
+  if( maxSeq(NULL, 0) != 0 ) return EXIT_FAILURE;
+  if( maxSeq(array1, 0) != 2 ) return EXIT_FAILURE;
+  if( maxSeq(array4, 6) != 5 ) return EXIT_FAILURE;
+  if( maxSeq(array4, 5) != 5 ) return EXIT_FAILURE;
   //if( doTest (array5, 3) != 1 ) return EXIT_FAILURE;
   //if( doTest (array6, 2) != 2 ) return EXIT_FAILURE;
   //doTest (array2, 0);
