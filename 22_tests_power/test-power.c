2f79333dd15d90408b121ec71986ea13e3e5f239
Coursera Learner <nobody@nowhere.nul>
2019-10-13 17:59:55 +0000

    did this

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+void doTest(unsigned x, unsigned y, unsighed ans){
+  if ( power( x, y ) == ans ){
+    exit(EXIT_SUCCESS);
+  }
+  else{
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(void){
+  doTest( 0, 0, 1);
+  doTest( 1, 0, 1);
+  doTest( 0, 1, 0);
+  doTest( 1.5, 0, EXIT_FAILURE);
+  return EXIT_SUCCESS;
+}
